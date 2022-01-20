90aae66a08613ba74d54431e59bee57f836f8322
Calvin Wong <calvinwong.tse@gmail.com>
2022-01-19 18:46:58 -0800

    insertion sort complete

--- Intro/Sort.py
+++ Intro/Sort.py
@@ -1,5 +1,14 @@
-class Solution(opject):
+class Solution(object):
     def insertionSort(self, n):
-
+        for j in range(len(n)):
+            key = n[j]
+            i = j - 1
+            while i >= 0 and n[i] > key:
+                n[i + 1] = n[i]
+                i = i - 1
+            n[i + 1] = key
         return n
 
+nums = [5,2,4,11,6,1,3,10]
+s = Solution()
+print(s.insertionSort(nums)) #4
