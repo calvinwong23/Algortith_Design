faad2a47ee6c1327cff2191f7dd0e347abb51e6b
Calvin Wong <calvinwong.tse@gmail.com>
2022-01-24 17:55:59 -0800

    Matrix Multiplication initial commit

---
+++ Intro/Divide and Conquer/matrixMultiplication.py
@@ -0,0 +1,17 @@
+class matrixMultiplication(object):
+    def nestedLoop(self, listA, listB):
+        
+        return 0
+
+A_list = [
+    [1,3],
+    [5,7]
+]
+
+B_list = [
+    [2,4],
+    [6,8]
+]
+
+m = matrixMultiplication()
+print(m.nestedLoop(A_list, B_list))
